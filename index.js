a04c34aac8c53d554bfc74c883f863ea04fc6256
ELMER MAURICIO MARULANDA OSORIO <elmer2164701@correo.uis.edu.co>
2022-02-14 14:35:24 -0500

    integrando base de datos con mongodb

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const Note = require("./models/note");
+/* const mongoose = require("mongoose"); */
 const app = express();
+/* const url =
+  "mongodb+srv://mauricio05:TMg6JiZmQXaR6Qy@cluster0.pstm5.mongodb.net/note-app?retryWrites=true&w=majority"; */
 
 app.use(cors());
 app.use(express.json());
@@ -12,7 +17,41 @@ morgan.token("content", (req, res) => {
 
 app.use(morgan(":method :url :status :response-time ms :content"));
 
-let notes = [
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
+  next(error);
+};
+app.use(errorHandler);
+
+/* 
+mongoose.connect(url);
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+}); */
+
+/* noteSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+}); */
+
+/* const Note = mongoose.model("Note", noteSchema); */
+
+/* let notes = [
   {
     id: 1,
     content: "HTML is easy",
@@ -31,21 +70,39 @@ let notes = [
     date: "2019-05-30T19:20:14.298Z",
     important: true,
   },
-];
+]; */
+
 app.get("/", (request, response) => {
   response.send("<h1>Hello world!</h1>");
 });
 app.get("/api/notes", (request, response) => {
-  response.json(notes);
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
+  /* response.json(notes); */
 });
-app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);
-  if (note) {
+app.get("/api/notes/:id", (request, response, next) => {
+  const id = request.params.id;
+  /*   const note = notes.find((note) => note.id === id); */
+  Note.findById(id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => {
+      next(error);
+      /* console.log(error);
+      response.status(400).send({ error: "malfomatted id" }); */
+    });
+
+  /* if (note) {
     response.json(note);
   } else {
     response.status(404).end();
-  }
+  } */
 });
 
 app.delete("/api/notes/:id", (request, response) => {
@@ -54,26 +111,39 @@ app.delete("/api/notes/:id", (request, response) => {
   response.status(204).end();
 });
 
-const generateId = () => {
+/* const generateId = () => {
   const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
   return maxId + 1;
-};
+}; */
 
 app.post("/api/notes", (request, response) => {
   const body = request.body;
+  if (body.content === undefined) {
+    return response.status(400).json({ error: "content missing" });
+  }
   if (!body.content) {
     return response.status(400).json({
       error: "content missing",
     });
   }
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId(),
-  };
-  notes = notes.concat(note);
-  response.json(note);
+    /* id: generateId(), */
+  });
+
+  note
+    .save()
+    .then((savedNote) => {
+      return savedNote.toJSON();
+    })
+    .then((savedAndFormattedNote) => {
+      response.json(savedAndFormattedNote);
+    })
+    .catch((error) => next(error));
+  /*   notes = notes.concat(note);
+  response.json(note); */
 });
 
 const PORT = process.env.PORT || 3001;
